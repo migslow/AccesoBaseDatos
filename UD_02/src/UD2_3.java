7ea1f934150d7a252ccee11a4373a037cc1d3fef
DAM2Alu10 <DAM2Alu10@B01-PC01.IESGUARA.LOCAL>
2023-09-18 09:19:23 +0200

    Ejercicio completado

--- UD_02/src/UD2_3.java
+++ UD_02/src/UD2_3.java
@@ -1,8 +1,43 @@
+import java.io.File;
+import java.io.IOException;
 
 public class UD2_3 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		boolean resultado;
+		File directorio = new File("C:\\DAM2");
+		File fichero = new File("C:\\DAM2\\alberto.txt");
+		resultado = directorio.mkdir();
+		if (resultado) {
+			System.out.println("El directorio se ha creado");
+		} else {
+			System.out.println("El directorio no se ha creado");
+			System.exit(-1);
+		}
+		try {
+			resultado = fichero.createNewFile();
+			if (resultado) {
+				System.out.println("Archivo creado");
+			} else {
+				System.out.println("No se ha podido crear el archivo");
+			}
+		} catch (IOException e) {
+			System.out.println("Se ha producido un error: " + e.getMessage());
+		}
+		
+		resultado = fichero.delete();
+		if (resultado) {
+			System.out.println("El fichero se ha eliminado");
+		} else {
+			System.out.println("El fichero no se ha podido eliminar");
+		}
+
+		resultado = directorio.delete();
+		if (resultado) {
+			System.out.println("El directorio se ha eliminado");
+		} else {
+			System.out.println("El directorio no se ha podido eliminar");
+		}
 
 	}
 
