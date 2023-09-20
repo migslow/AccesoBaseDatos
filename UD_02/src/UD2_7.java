bb4e8f2237612f361ab2c5f10ebfa98ab187a90d
DAM2Alu10 <DAM2Alu10@B01-PC01.IESGUARA.LOCAL>
2023-09-20 10:15:15 +0200

    a

--- UD_02/src/UD2_7.java
+++ UD_02/src/UD2_7.java
@@ -1,23 +1,28 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class UD2_7 {
 
 	public static void main(String[] args) {
+		int contador = 0;
 		try {
-			BufferedWriter bw = new BufferedWriter(new FileWriter("Ficheros\\puntuacion.dat"));
-			BufferedReader br = new BufferedReader(new FileReader("Ficheros\\puntuacion.dat"));
+			DataOutputStream dos = new DataOutputStream(new FileOutputStream("Ficheros\\puntuacion.dat"));
+			DataInputStream dis = new DataInputStream(new FileInputStream("Ficheros\\puntuacion.dat"));
+			int aux;
 			for (int i = 0; i <= 20; i++) {
-				int numeros = (int) (Math.random() * 5 + 1);
-				bw.write(numeros);
+				aux = (int) (Math.random() * 5 + 1);
+				dos.writeInt(aux);
 			}
-			bw.close();
-			
-			
+			dos.close();
+
+			while (true) {
+				System.out.println("Numero: " + dis.readInt());
+			}
+
 		} catch (Exception e) {
-			System.out.println("A ocurrido un problema de entrada y salida");
+			System.out.println("A ocurrido un problema de E/S");
 		}
 
 	}
